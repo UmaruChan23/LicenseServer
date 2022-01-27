feff693c77b5bc9cff6f8b8124eb415c104f1f48
fmatorin <f.matorin@mail.ru>
2022-01-27 14:55:50 +0300

    Исправлено имя файла сертификата

--- src/main/java/com/server/licenseserver/model/Ticket.java
+++ src/main/java/com/server/licenseserver/model/Ticket.java
@@ -2,12 +2,12 @@ package com.server.licenseserver.model;
 
 import com.server.licenseserver.entity.License;
 import lombok.Data;
+import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaCertStore;
-import org.bouncycastle.cms.CMSProcessableByteArray;
-import org.bouncycastle.cms.CMSSignedData;
-import org.bouncycastle.cms.CMSSignedDataGenerator;
-import org.bouncycastle.cms.CMSTypedData;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cms.*;
 import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
+import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
@@ -20,10 +20,7 @@ import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.*;
 
 @Data
 public class Ticket {
@@ -45,8 +42,6 @@ public class Ticket {
         this.deviceId = license.getDeviceId();
         this.type = license.getCode().getType();
         this.blocked = false;
-
-        //create signature
     }
 
     public void signTicket() {
@@ -63,7 +58,7 @@ public class Ticket {
 
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         char[] keyStorePassword = "changeit".toCharArray();
-        keyStore.load(new FileInputStream("newKeyStoreName"), keyStorePassword);
+        keyStore.load(new FileInputStream("diplomKeyStore"), keyStorePassword);
 
         KeyStore.ProtectionParameter entryPassword =
                 new KeyStore.PasswordProtection(keyStorePassword);
@@ -85,6 +80,22 @@ public class Ticket {
         gen.addSignerInfoGenerator(new JcaSignerInfoGeneratorBuilder(new JcaDigestCalculatorProviderBuilder().setProvider("BC").build()).build(signer, (X509Certificate) certificates[0]));
         gen.addCertificates(certStore);
         CMSSignedData sigData = gen.generate(msg, true);
+
+        Store store = sigData.getCertificates();
+        SignerInformationStore signers = sigData.getSignerInfos();
+        Collection c = signers.getSigners();
+        Iterator it = c.iterator();
+        while (it.hasNext()) {
+            SignerInformation sig = (SignerInformation) it.next();
+            Collection certCollection = store.getMatches(sig.getSID());
+            Iterator certIt = certCollection.iterator();
+            X509CertificateHolder certHolder = (X509CertificateHolder) certIt.next();
+            X509Certificate cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(certHolder);
+            if (sig.verify(new JcaSimpleSignerInfoVerifierBuilder().setProvider("BC").build(cert))) {
+                System.out.println("verified");
+            }
+        }
+
 
         return sigData.getEncoded();
     }
