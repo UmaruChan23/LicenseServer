b2ca833eb08fdc0e946c016443832b33a85fbde5
fmatorin <f.matorin@mail.ru>
2022-01-31 17:25:48 +0300

    Решена гонка счетчика кода активации

--- src/main/java/com/server/licenseserver/repo/ActivationCodeRepo.java
+++ src/main/java/com/server/licenseserver/repo/ActivationCodeRepo.java
@@ -2,7 +2,11 @@ package com.server.licenseserver.repo;
 
 import com.server.licenseserver.entity.ActivationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import javax.persistence.LockModeType;
 
 public interface ActivationCodeRepo extends JpaRepository<ActivationCode, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     ActivationCode findByCode(String code);
 }
