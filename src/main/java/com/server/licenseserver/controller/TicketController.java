808199edbd885c4476620783554c59d426ec9ebd
fmatorin <f.matorin@mail.ru>
2022-02-07 15:35:34 +0300

    Фиксы тикетов

--- src/main/java/com/server/licenseserver/controller/TicketController.java
+++ src/main/java/com/server/licenseserver/controller/TicketController.java
@@ -1,6 +1,6 @@
 package com.server.licenseserver.controller;
 
-import com.server.licenseserver.entity.License;
+import com.server.licenseserver.exception.handler.InvalidTicketException;
 import com.server.licenseserver.model.Ticket;
 import com.server.licenseserver.service.LicenseService;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/ticket")
@@ -19,13 +21,8 @@ public class TicketController {
         this.licenseService = licenseService;
     }
 
-    //1)Проверить наличие лицензий в базе по deviceId
-    //2)Найти не заблокиованную лицензию из списка
-    //3)Проверить даты тикета и базы
     @PostMapping
-    public ResponseEntity<Ticket> renewTicket(@RequestBody Ticket ticket) {
-
-        //проверка подписи тикета
+    public ResponseEntity<Ticket> renewTicket(@Valid @RequestBody Ticket ticket) throws InvalidTicketException {
         Ticket newTicket = licenseService.refreshTicket(ticket);
         newTicket.signTicket();
         return ResponseEntity.ok(newTicket);
---
+++ src/main/java/com/server/licenseserver/exception/handler/InvalidTicketException.java
@@ -0,0 +1,5 @@
+package com.server.licenseserver.exception.handler;
+
+public class InvalidTicketException extends Exception {
+    public InvalidTicketException (String msg) { super(msg); }
+}
