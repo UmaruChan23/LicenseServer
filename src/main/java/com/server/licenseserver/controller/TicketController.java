b497cb010be772def8885d59106ee9ba62a2a0ea
fmatorin <f.matorin@mail.ru>
2022-02-20 19:13:43 +0300

    Снова тикеты

--- src/main/java/com/server/licenseserver/controller/TicketController.java
+++ src/main/java/com/server/licenseserver/controller/TicketController.java
@@ -1,13 +1,14 @@
 package com.server.licenseserver.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.server.licenseserver.exception.handler.InvalidTicketException;
 import com.server.licenseserver.model.Ticket;
 import com.server.licenseserver.security.jwt.JwtProvider;
 import com.server.licenseserver.service.LicenseService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/ticket")
@@ -23,9 +24,13 @@ public class TicketController {
     }
 
     @PostMapping
+    @ResponseBody
     public ResponseEntity<Ticket> renewTicket(@RequestHeader("Authorization") String token,
-                                              @RequestBody long productId)
-            throws InvalidTicketException {
+                                              @RequestBody String json)
+            throws InvalidTicketException, JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(json);
+        long productId = root.path("productId").asLong();
         String subToken = token.substring(7);
         String login = jwtProvider.getLoginFromToken(subToken);
         String deviceId = jwtProvider.getDeviceIdFromToken(subToken);
